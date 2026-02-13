79e25f2226233e13731f825ed555c4775b4c4db7
TheStranjer <791672+TheStranjer@users.noreply.github.com>
2026-02-12 18:25:02 -0700

    Guilds subdirectory for tools

--- lib/fracture.rb
+++ lib/fracture.rb
@@ -6,9 +6,9 @@ require 'mcp'
 require_relative 'fracture/patches/windows_schema'
 require_relative 'fracture/discord_client'
 require_relative 'fracture/server'
-require_relative 'fracture/tools/get_guilds'
-require_relative 'fracture/tools/get_guild'
-require_relative 'fracture/tools/get_guild_members'
+require_relative 'fracture/tools/guilds/get_guilds'
+require_relative 'fracture/tools/guilds/get_guild'
+require_relative 'fracture/tools/guilds/get_guild_members'
 
 module Fracture
 end
